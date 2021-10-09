c042147d1d01c16e6dafdf7cbc6f96c3639ff92d
danghung-dev <danghung.dev@gmail.com>
2021-10-09 22:34:12 +0700

    add get set

--- backend/src/main/java/vn/dev/danghung/entities/User.java
+++ backend/src/main/java/vn/dev/danghung/entities/User.java
@@ -3,4 +3,11 @@ package vn.dev.danghung.entities;
 public class User {
     private String username;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
